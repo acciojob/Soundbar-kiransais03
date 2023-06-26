bd16f54fa55de058b1b438fed9dcfda36638c2f6
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-06-26 14:05:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let audio1;
+document.getElementById('applause').addEventListener('click',makeasound);
+document.getElementById('boo').addEventListener('click',makeasound);
+document.getElementById('gasp').addEventListener('click',makeasound);
+document.getElementById('tada').addEventListener('click',makeasound);
+document.getElementById('victory').addEventListener('click',makeasound);
+document.getElementById('wrong').addEventListener('click',makeasound);
+document.getElementById('stop').addEventListener('click',stopasound);
+
+function makeasound(event) {
+	let name=event.id;
+	audio1 = new Audio(`./sounds/${name}`);
+	audio1.play();
+}
+
+function stopasound(event) {
+	audio1.stop();
+}
